9d1c1bfaeaa3e1d1b99c0dd1e3eac188e4f4174d
Gyanendra Kumar <gyanendra.kumar@publicisresources.com>
2023-09-09 02:32:00 +0530

    Added theme

--- Home.py
+++ Home.py
@@ -1,5 +1,17 @@
 import streamlit as st
 from utils import add_marcel_logo
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 st.set_page_config(
     page_title="Marcel Chatbot",
     page_icon='💬',
